44ccc22db0081926cc9f681ca48d361640f2a895
quarriz <quarriz@3il.fr>
2023-11-23 08:38:24 +0100

    Resolution du debogage

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // Problème 1
         int[] numbers = {1, 2, 3, 4, 5};
-        for (int i = 0; i <= numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             System.out.println("Number at index " + i + ": " + numbers[i]);
         }
 
@@ -11,8 +11,8 @@ public class Main {
         System.out.println("Result: " + result);
 
         // Problème 3
-        int count = 10;
-        while (count > 0) {
+        int count = 1;
+        while (count < 10) {
             System.out.println("Count: " + count);
             count += 2;
         }
@@ -20,26 +20,32 @@ public class Main {
         // Problème 4
         String message = "Bonjour";
         System.out.println("Longueur du message : " + message.length());
-        char character = message.charAt(10);
-        System.out.println("Caractère à l'indice 10 : " + character);
+        char character = message.charAt(1);
+        System.out.println("Caractère à l'indice 1 : " + character);
 
         // Problème 5
         String str1 = "Hello";
         String str2 = new String("Hello");
-        System.out.println("Comparaison de chaînes : " + (str1 == str2));
+        System.out.println("Comparaison de chaînes : " + str1.equals(str2));
 
         // Problème 6
-        Main mainObj = null;
+        Main mainObj = new Main();
         mainObj.method();
     }
 
     // Méthode pour le problème de division par zéro
     public static int calculateValue(int a, int b) {
-        return a / b;
+        if (b == 0)
+        {
+        	System.out.println("Pas possible de diviser par 0");
+        	return 0;
+        }
+    	return a / b;
     }
 
     // Méthode pour le problème d'utilisation incorrecte d'une méthode de classe
-    public void method() {
+    public static void method() {
         System.out.println("Méthode appelée !");
+        
     }
 }
